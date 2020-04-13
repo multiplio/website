b827c692762620bdde86fbf8278c5792a1817d5a
Martin Toman <whomenope@outlook.com>
2020-04-13 15:52:14 +0200

    Use react-static automatic Routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from 'react'
-import { Root, addPrefetchExcludes } from 'react-static'
+import { Root, Routes } from 'react-static'
 //
 import { Router } from 'components/Router'
 
 import './app.css'
-
-// pages
-import Home from './pages/Home'
-import PP from './pages/PrivacyPolicy'
-import TOS from './pages/TermsOfUse'
-
-// Any routes that start with 'dynamic' will be treated as non-static routes
-addPrefetchExcludes(['dynamic'])
 
 function App() {
   return (
@@ -19,9 +11,7 @@ function App() {
       <main>
         <React.Suspense fallback={<em>Loading...</em>}>
           <Router>
-            <Route exact path='/' component={Home} />
-            <Route path='/privacy' component={PP} />
-            <Route path='/terms' component={TOS} />
+            <Routes path="*" />
           </Router>
         </React.Suspense>
       </main>
